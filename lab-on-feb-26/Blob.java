78f63e37694f3b8c026c09f8e4c6ea4d2ada6dce
c212 <40395648+c212@users.noreply.github.com>
2021-02-26 16:34:29 -0500

    make blob a type of node
    we inherit from node

--- lab-on-feb-26/Blob.java
+++ lab-on-feb-26/Blob.java
@@ -1,23 +1,15 @@
-public class Blob {
-  int key;
-  Blob left, right; 
-  public Blob(int key, Blob left, Blob right) {
-    this.key = key; 
-    this.left = left; 
-    this.right = right; 
+public class Blob extends Node {
+  // constructor chaining
+  public Blob(int key, Node left, Node right) {
+    super(key, left, right);  
   }
   public Blob(int key) {
-    // how do I describe this one in terms of the other constructor 
-    this(key, null, null); // this is how you do it    
-  }
-  // how do you propose to print this structure? 
-  public String toString() {
-    return "( " + this.key + " " + this.left + " " + this.right + " )" ;  
+    super(key);     
   }
   public static void main(String[] args) {
     Blob a = new Blob(1, new Blob(2, null, null), null);  
     Blob b = new Blob(3); // new Blob(3, null, null); 
     a.right = b; 
-    System.out.println(a); 
+    a.display();
   }
 }
